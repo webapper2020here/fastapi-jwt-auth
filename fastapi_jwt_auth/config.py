7b9b2376bb62721c483eb2b1a768b79fef415ad5
webapper2020here <websterappproger2020@proton.me>
2024-09-27 17:44:20 +0200

    Update config.py

--- fastapi_jwt_auth/config.py
+++ fastapi_jwt_auth/config.py
@@ -9,7 +9,7 @@ from pydantic import (
 )
 
 class LoadConfig(BaseModel):
-    authjwt_token_location: Optional[Sequence[StrictStr]] = {'headers'}
+    authjwt_token_location: Optional[List[StrictStr]] = ['headers']
     authjwt_secret_key: Optional[StrictStr] = None
     authjwt_public_key: Optional[StrictStr] = None
     authjwt_private_key: Optional[StrictStr] = None
@@ -20,12 +20,12 @@ class LoadConfig(BaseModel):
     authjwt_decode_issuer: Optional[StrictStr] = None
     authjwt_decode_audience: Optional[Union[StrictStr,Sequence[StrictStr]]] = None
     authjwt_denylist_enabled: Optional[StrictBool] = False
-    authjwt_denylist_token_checks: Optional[Sequence[StrictStr]] = {'access','refresh'}
+    authjwt_denylist_token_checks: Optional[List[StrictStr]] = ['access','refresh']
     authjwt_header_name: Optional[StrictStr] = "Authorization"
     authjwt_header_type: Optional[StrictStr] = "Bearer"
     authjwt_access_token_expires: Optional[Union[StrictBool,StrictInt,timedelta]] = timedelta(minutes=15)
     authjwt_refresh_token_expires: Optional[Union[StrictBool,StrictInt,timedelta]] = timedelta(days=30)
-    # option for create cookies
+    # # option for create cookies
     authjwt_access_cookie_key: Optional[StrictStr] = "access_token_cookie"
     authjwt_refresh_cookie_key: Optional[StrictStr] = "refresh_token_cookie"
     authjwt_access_cookie_path: Optional[StrictStr] = "/"
@@ -34,7 +34,7 @@ class LoadConfig(BaseModel):
     authjwt_cookie_domain: Optional[StrictStr] = None
     authjwt_cookie_secure: Optional[StrictBool] = False
     authjwt_cookie_samesite: Optional[StrictStr] = None
-    # option for double submit csrf protection
+    # # option for double submit csrf protection
     authjwt_cookie_csrf_protect: Optional[StrictBool] = True
     authjwt_access_csrf_cookie_key: Optional[StrictStr] = "csrf_access_token"
     authjwt_refresh_csrf_cookie_key: Optional[StrictStr] = "csrf_refresh_token"
@@ -42,7 +42,7 @@ class LoadConfig(BaseModel):
     authjwt_refresh_csrf_cookie_path: Optional[StrictStr] = "/"
     authjwt_access_csrf_header_name: Optional[StrictStr] = "X-CSRF-Token"
     authjwt_refresh_csrf_header_name: Optional[StrictStr] = "X-CSRF-Token"
-    authjwt_csrf_methods: Optional[Sequence[StrictStr]] = {'POST','PUT','PATCH','DELETE'}
+    authjwt_csrf_methods: Optional[List[StrictStr]] = ['POST','PUT','PATCH','DELETE']
 
     @validator('authjwt_access_token_expires')
     def validate_access_token_expires(cls, v):
@@ -76,10 +76,10 @@ class LoadConfig(BaseModel):
 
     @validator('authjwt_csrf_methods', each_item=True)
     def validate_csrf_methods(cls, v):
-        if v.upper() not in {"GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"}:
+        if v.upper() not in ["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"]:
             raise ValueError("The 'authjwt_csrf_methods' must be between http request methods")
         return v.upper()
 
     class Config:
-        min_anystr_length = 1
-        anystr_strip_whitespace = True
+        str_min_length = 1
+        str_strip_whitespace = True
